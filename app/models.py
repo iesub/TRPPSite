038c62335c750290bb1af3a65e2741c019320d41
User <psh030302@gmail.com>
2021-04-04 12:31:06 +0300

    дизайн и сортировка чатов по времени

---
+++ app/models.py
@@ -0,0 +1,158 @@
+"""
+модуль models
+"""
+
+import pytz
+from datetime import datetime
+from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import login
+from hashlib import md5
+
+
+followers = db.Table('followers',
+                     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+                     db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+                     )
+
+
+class User(UserMixin, db.Model):
+    """
+    Таблица пользователя
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    invitations = db.relationship('Invitation', backref='user', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.now(pytz.timezone('Europe/Moscow')))
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        """
+        метод установки пароля
+
+        :param password: пароль
+        :type password: строка
+        :return: ничего не возвращает
+        """
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        """
+        метод проверки пароля
+
+        :param password: пароль
+        :type password: строка
+        :return: True или False
+        """
+        return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        """
+        метод утображения объекта аватра пользователя
+
+        :param size: размер картинки
+        :type size: число
+        :return: аватар пользователя
+        """
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
+
+    def follow(self, user):
+        """
+        метод добавления пользователя в список друзей
+
+        :param user: пользователь, которого нужно добавить
+        :type user: пользователь
+        :return: ничено не возврщает
+        """
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        """
+        метод удаления пользователя в список друзей
+
+        :param user: пользователь, которого нужно удалить
+        :type user: пользователь
+        :return: ничено не возврщает
+        """
+        if self.is_following(user):
+            self.followed.remove(user)
+        elif user.is_following(self):
+            user.followed.remove(self)
+
+    def is_following(self, user):
+        """
+        метод проверки, является пользователь другом
+
+        :param user: пользователь, которого нужно проверить
+        :type user: пользователь
+        :return: True или False
+        """
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
+
+
+@login.user_loader
+def load_user(id):
+    """
+    метод загрузки пользователя из бд
+
+    :param id: id пользователя
+    :type id: число
+    :return: пользователь
+    """
+    return User.query.get(int(id))
+
+
+class Post(db.Model):
+    """
+    таблица сообщений пользователей
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.now(pytz.timezone('Europe/Moscow')))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    chat_id = db.Column(db.Integer, db.ForeignKey('chat.id'))
+
+    def __repr__(self):
+        return '{}'.format(self.body)
+
+
+class Invitation(db.Model):
+    """
+    таблица приглашений пользователей в чаты
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    chat_id = db.Column(db.Integer, db.ForeignKey('chat.id'))
+
+
+user_chats = db.Table('user_chats',
+                      db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+                      db.Column('chat_id', db.Integer, db.ForeignKey('chat.id'))
+                      )
+
+
+class Chat(db.Model):
+    """
+    таблица чатов
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True)
+    users = db.relationship('User', secondary=user_chats, backref='chats')
+    posts = db.relationship('Post', backref='chat', lazy='dynamic')
+    invitations = db.relationship('Invitation', backref='chat', lazy='dynamic')
---
+++ app/routes.py
@@ -0,0 +1,345 @@
+# -*- coding: utf-8 -*-
+from flask import render_template, flash, redirect, url_for, request
+from werkzeug.urls import url_parse
+from app import app, db
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, SearchAndCreateChatForm, SearchUserForm, PostForm
+from flask_login import current_user, login_user
+from app.models import User, Chat, Post, Invitation
+from flask_login import logout_user, login_required
+from datetime import datetime
+import pytz
+
+
+"""
+модуль routes
+"""
+
+
+@app.before_request
+def before_request():
+    """
+    метод определения времени последнего действия пользователя
+
+    :return: ничего не возвращает
+    """
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.now(pytz.timezone('Europe/Moscow'))
+        db.session.commit()
+
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
+@login_required
+def index():
+    """
+    метод отбражения главной страницы сайта
+
+    :return: главная страница
+    """
+    form = SearchAndCreateChatForm()
+    if form.validate_on_submit():
+        if form.create_name.data == '':
+            chat = Chat.query.filter_by(name=form.search_name.data).first()
+            if chat in current_user.chats:
+                return redirect(url_for('chat', name=chat.name))
+        if form.search_name.data == '':
+            chat = Chat.query.filter_by(name=form.create_name.data).first()
+            if chat is None:
+                chat = Chat(name=form.create_name.data)
+                chat.users.append(current_user)
+                db.session.add(chat)
+                post = Post(body=f'{current_user.username} создал чат', author=current_user, chat=chat,
+                            timestamp=datetime.now(pytz.timezone('Europe/Moscow')))
+                db.session.add(post)
+                db.session.commit()
+                flash('Поздравляю, вы создали новую беседу!')
+                return redirect(url_for('chat', name=chat.name))
+    chats = current_user.chats
+    chats.sort(key=lambda x: x.posts[-1].timestamp)
+    invite = Invitation.query.filter_by(user=current_user).first()
+    if invite is None:
+        return render_template('index.html', title='Главная страница', chats=reversed(chats), count=0,
+                               form=form)
+    return render_template('index.html', title='Главная страница', chats=reversed(chats),
+                           invitations=current_user.invitations, count=1, form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    """
+    метод отбражения страницы авторизации
+
+    :return: страница авторизации или переход на метод index()
+    """
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Неправильные имя пользователя или пароль')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='Войти', form=form)
+
+
+@app.route('/logout')
+def logout():
+    """
+    метод выхода из акаунта
+
+    :return: переход на главную страницу
+    """
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    """
+    метод отбражения страницы регистрации
+
+    :return: страница авторизации или переход на метод index()
+    """
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Поздравляю, теперь вы зарегистрированный пользователь!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    """
+    метод отбражеия профиля пользователя
+
+    :param username: имя пользователя
+    :type username: строка
+    :return: страница профиля пользователя
+    """
+    user = User.query.filter_by(username=username).first_or_404()
+    chats = user.chats
+    return render_template('user.html', user=user, chats=chats, title=user.username)
+
+
+@app.route('/write_message/<username>')
+@login_required
+def write_message(username):
+    """
+    метод перехода на страницу личной переписки с пльзователем
+
+    :param username: имя пользователя
+    :type username: строка
+    :return: переход на метод chat(name)
+    """
+    user = User.query.filter_by(username=username).first_or_404()
+    chat = Chat.query.filter_by(name='none'+user.username+current_user.username).first()
+    if chat is None:
+        chat = Chat.query.filter_by(name='none' + current_user.username + user.username).first()
+    if chat is None:
+        chat = Chat(name='none' + user.username + current_user.username)
+        chat.users.append(current_user)
+        chat.users.append(user)
+        post = Post(body=f'{current_user.username} создал чат', author=current_user, chat=chat,
+                    timestamp=datetime.now(pytz.timezone('Europe/Moscow')))
+        db.session.add(post)
+        db.session.commit()
+    return redirect(url_for('chat', name=chat.name))
+
+
+@app.route('/chat/<name>', methods=['GET', 'Post'])
+@login_required
+def chat(name):
+    """
+    метод отбражения страницы чата
+
+    :param name: название чата
+    :type name: строка
+    :return: страница чата
+    """
+    form = PostForm()
+    chat = Chat.query.filter_by(name=name).first()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user, chat=chat,
+                    timestamp=datetime.now(pytz.timezone('Europe/Moscow')))
+        db.session.add(post)
+        db.session.commit()
+        return redirect(url_for('chat', name=chat.name))
+    posts = chat.posts
+    return render_template("chat.html", title='Home Page', form=form, chat=chat,
+                           posts=posts)
+
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    """
+    метод отбражения страницы изменения данных пользователя
+
+    :return: страница изменения данных пользователя или переход на метод user(username)
+    """
+    form = EditProfileForm(current_user.username)
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Ваши изменения были сохранены.')
+        return redirect(url_for('user', username=current_user.username))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Изменение профиля',
+                           form=form)
+
+
+@app.route('/invite_user/<name>', methods=['GET', 'POST'])
+@login_required
+def invite_user(name):
+    """
+    метод приглашения аользователя в чат
+
+    :param name: название чата
+    :type name: строка
+    :return: страница поиска пользователя или переход на метод chat(name)
+    """
+    form = SearchUserForm()
+    chat = Chat.query.filter_by(name=name).first()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None:
+            flash('извините, пользователя с таким именем не существует')
+            return redirect(url_for('search_user'))
+        invitation = Invitation()
+        invitation.user = user
+        invitation.chat = chat
+        db.session.add(invitation)
+        db.session.commit()
+        flash('Поздравляю, вы приглосили нового пользователя!')
+        return redirect(url_for('chat', name=name))
+    return render_template('search_user.html', title='Поиск беседы', form=form)
+
+
+@app.route('/accept_the_invitation/<invitation_id>')
+@login_required
+def accept_the_invitation(invitation_id):
+    """
+    метод принятия приглашения
+
+    :param invitation_id: id приглашения
+    :type invitation_id: строка
+    :return: переход на метод index()
+    """
+    invitation = Invitation.query.filter_by(id=int(invitation_id)).first()
+    current_user.chats.append(invitation.chat)
+    db.session.delete(invitation)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+@app.route('/decline_the_invitation/<invitation_id>')
+@login_required
+def decline_the_invitation(invitation_id):
+    """
+    метод отклонения приглашения
+
+    :param invitation_id: id приглашения
+    :type invitation_id: строка
+    :return: переход на метод index()
+    """
+    invitation = Invitation.query.filter_by(id=int(invitation_id)).first()
+    db.session.delete(invitation)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+@app.route('/follow/<username>')
+@login_required
+def follow(username):
+    """
+    метод добавления пользователя в список друзей
+
+    :param username: имя пользователя
+    :type username: строка
+    :return: переход на метод index()
+    """
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('Пользователь {} не найден.'.format(username))
+        return redirect(url_for('index'))
+    if user == current_user:
+        flash('Вы не можете подружиться с самим собой')
+        return redirect(url_for('user', username=username))
+    current_user.follow(user)
+    db.session.commit()
+    return redirect(url_for('user', username=username))
+
+
+@app.route('/unfollow/<username>')
+@login_required
+def unfollow(username):
+    """
+    метод удаления пользователя из списока друзей
+
+    :param username: имя пользователя
+    :type username: строка
+    :return: переход на метод index()
+    """
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('Пользователь {} не найден.'.format(username))
+        return redirect(url_for('index'))
+    if user == current_user:
+        flash('Вы не можете подружиться с самим собой.')
+        return redirect(url_for('user', username=username))
+    current_user.unfollow(user)
+    db.session.commit()
+    return redirect(url_for('user', username=username))
+
+
+@app.route('/list_of_friends', methods=['GET', 'POST'])
+@login_required
+def list_of_friends():
+    """
+    метод отбражения страницы списка друзей
+
+    :return: страница списка друзей
+    """
+    users = User.query.all()
+    followers = []
+    followed = []
+    for user in users:
+        if user.is_following(current_user):
+            followers.append(user)
+        elif current_user.is_following(user):
+            followed.append(user)
+    form = SearchUserForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None:
+            flash('извините, пользователя с таким именем не существует')
+            return redirect(url_for('search_user'))
+        return redirect(url_for('user', username=user.username))
+    return render_template('list_of_friends.html', title='Список друзей', followers=followers, followed=followed,
+                           form=form)
+
+
+@app.route('/news')
+@login_required
+def news():
+    """
+    метод отбражения страницы новостей
+
+    :return: страница новостей
+    """
+    return render_template('news.html', title='Новости')
